f278279a133cebafc89aae2ed0429d9a5a99f366
Sumit Anand <anandsumit2000@gmail.com>
2026-01-07 13:45:02 +0530

    feat: add Grafana role assignments for IDP resource monitoring

--- terraform/05-observability/outputs.tf
+++ terraform/05-observability/outputs.tf
@@ -29,3 +29,13 @@ output "action_group_id" {
   description = "ID of the monitor action group"
   value       = azurerm_monitor_action_group.main.id
 }
+
+output "grafana_url" {
+  description = "URL for accessing Grafana dashboard"
+  value       = azurerm_dashboard_grafana.main.endpoint
+}
+
+output "grafana_id" {
+  description = "Resource ID of the Grafana dashboard"
+  value       = azurerm_dashboard_grafana.main.id
+}
