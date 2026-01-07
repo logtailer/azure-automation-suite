cc57defc6fd486d0803ab561de4328470d083a81
Sumit Anand <anandsumit2000@gmail.com>
2026-01-07 18:56:47 +0530

    feat: add enterprise network security with NSGs and private endpoints

--- terraform/03-security/variables.tf
+++ terraform/03-security/variables.tf
@@ -76,3 +76,40 @@ variable "custom_role_assignments" {
   }))
   default = {}
 }
+
+# Network Security Configuration
+variable "enable_network_security" {
+  description = "Enable network security group for security resources"
+  type        = bool
+  default     = true
+}
+
+variable "enable_kv_network_restriction" {
+  description = "Enable network restrictions on Key Vault"
+  type        = bool
+  default     = false
+}
+
+variable "allowed_ip_ranges" {
+  description = "List of allowed IP ranges for Key Vault access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"] # Default allows all, should be restricted in production
+}
+
+variable "enable_private_endpoint" {
+  description = "Enable private endpoint for Key Vault"
+  type        = bool
+  default     = false
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for private endpoint (required if enable_private_endpoint is true)"
+  type        = string
+  default     = ""
+}
+
+variable "vnet_id" {
+  description = "VNet ID for private DNS zone link (required if enable_private_endpoint is true)"
+  type        = string
+  default     = ""
+}
