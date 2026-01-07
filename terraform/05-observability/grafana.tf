d8e4d75e66ae3f3b05786fbea1e33ebc670ce283
Sumit Anand <anandsumit2000@gmail.com>
2026-01-07 13:45:02 +0530

    feat: add Grafana role assignments for IDP resource monitoring

--- terraform/05-observability/grafana.tf
+++ terraform/05-observability/grafana.tf
@@ -26,3 +26,12 @@ resource "azurerm_role_assignment" "grafana_data_reader" {
   role_definition_name = "Log Analytics Reader"
   principal_id         = azurerm_dashboard_grafana.main.identity.0.principal_id
 }
+
+# Grant Grafana Monitoring Reader access to IDP resource group
+# This allows Grafana to query metrics from Container Instances and PostgreSQL
+resource "azurerm_role_assignment" "grafana_idp_reader" {
+  count                = var.enable_idp_monitoring ? 1 : 0
+  scope                = data.terraform_remote_state.idp[0].outputs.resource_group_id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azurerm_dashboard_grafana.main.identity.0.principal_id
+}
