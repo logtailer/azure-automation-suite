0b6adbf66f1e109440084bc076641fbe1ae343b8
Sumit Anand <anandsumit2000@gmail.com>
2026-01-08 16:43:35 +0530

    fix: correct Azure Files storage_account_name attribute

--- terraform/08-artifactory/main.tf
+++ terraform/08-artifactory/main.tf
@@ -61,13 +61,13 @@ resource "azurerm_role_assignment" "acr_pull" {
 
 # Storage Account for Nexus persistent data
 resource "azurerm_storage_account" "nexus_data" {
-  name                     = "stnexusdata${var.environment}${random_id.storage_suffix.hex}"
-  resource_group_name      = data.azurerm_resource_group.main.name
-  location                 = data.azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = element(split("_", var.storage_account_sku), 1)
-  min_tls_version          = "TLS1_2"
-  https_traffic_only_enabled = true
+  name                            = "stnexusdata${var.environment}${random_id.storage_suffix.hex}"
+  resource_group_name             = data.azurerm_resource_group.main.name
+  location                        = data.azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = element(split("_", var.storage_account_sku), 1)
+  min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
 
   tags = var.tags
@@ -76,7 +76,7 @@ resource "azurerm_storage_account" "nexus_data" {
 # Azure Files share for /nexus-data persistent storage
 resource "azurerm_storage_share" "nexus_data" {
   name                 = "nexus-data"
-  storage_account_id   = azurerm_storage_account.nexus_data.id
+  storage_account_name = azurerm_storage_account.nexus_data.name
   quota                = var.file_share_quota
 
   depends_on = [azurerm_storage_account.nexus_data]
