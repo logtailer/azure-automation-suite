6cb06b521888239841976391d2ad64aeb1940024
Sumit Anand <anandsumit2000@gmail.com>
2026-01-25 11:29:44 +0530

    chore: upgrade terraform and azurerm versions in artifactory module

--- terraform/08-artifactory/main.tf
+++ terraform/08-artifactory/main.tf
@@ -1,16 +1,18 @@
 # Terraform configuration
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
-  # Backend configuration will be provided via -backend-config during init
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
