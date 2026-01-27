3b8a98bdc4012a5c10102da401332adcb288c45b
Sumit Anand <anandsumit2000@gmail.com>
2026-01-27 13:37:42 +0530

    feat: add subscription and allowed locations variables to governance

--- terraform/09-governance/variables.tf
+++ terraform/09-governance/variables.tf
@@ -1,3 +1,14 @@
+variable "subscription_id" {
+  description = "Azure subscription ID for policy assignments"
+  type        = string
+}
+
+variable "allowed_locations" {
+  description = "List of allowed Azure regions for deployments"
+  type        = list(string)
+  default     = ["eastus", "eastus2", "westus2", "westeurope"]
+}
+
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
