0e5ca5dd350b544f982775097c93fba3ce6dac56
Sumit Anand <anandsumit2000@gmail.com>
2026-03-09 09:50:00 +0530

    feat(observability): add Application Insights workbook for platform overview

---
+++ terraform/05-observability/workbook.tf
@@ -0,0 +1,15 @@
+resource "azurerm_application_insights_workbook" "platform_overview" {
+  name                = "550f9e53-1234-4567-abcd-platform-overview"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  display_name        = "Platform Overview"
+
+  data_json = jsonencode({
+    version        = "Notebook/1.0"
+    items          = []
+    styleSettings  = {}
+    "$schema"      = "https://github.com/Microsoft/Application-Insights-Workbooks/blob/master/schema/workbook.json"
+  })
+
+  tags = var.tags
+}
