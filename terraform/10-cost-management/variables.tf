a199debbdd6d32a1bc054f0043079b144851ee08
Sumit Anand <anandsumit2000@gmail.com>
2026-06-21 16:10:00 +0530

    fix(10-cost-management): set subscription budget default to 50 for student subscription

--- terraform/10-cost-management/variables.tf
+++ terraform/10-cost-management/variables.tf
@@ -34,5 +34,5 @@ variable "tags" {
 variable "subscription_budget_amount" {
   description = "Total monthly budget for the subscription in USD"
   type        = number
-  default     = 5000
+  default     = 50
 }
